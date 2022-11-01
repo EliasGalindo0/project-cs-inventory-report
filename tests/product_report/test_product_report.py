be4eb5aa846df122e807ddd93b3ebb8b1a975413
EliasGalindo0 <eliasgalindo@gmail.com>
2022-10-31 22:25:17 -0300

    feat: test_product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+      1, 'jPhone', 'Orange', '01/02/2003', '01/02/2004', 'JKLMNO123',
+      'Manter afastado do fogo'
+    )
+
+    assert str(product) == (
+      f"O produto {product.nome_do_produto}"
+      f" fabricado em {product.data_de_fabricacao}"
+      f" por {product.nome_da_empresa} com validade"
+      f" até {product.data_de_validade}"
+      f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
