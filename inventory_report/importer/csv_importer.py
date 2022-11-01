e0fce5b58e96bbb2dfac0ea8cc9af152db7c2cba
EliasGalindo0 <eliasgalindo@gmail.com>
2022-10-31 22:09:27 -0300

    feat: test_product

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -1,3 +1,2 @@
 def importer_csv():
     pass
-  
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+      1, 'jPhone', 'Orange', '01/02/2003', '01/02/2004', 'JKLMNO123',
+      'Manter afastado do fogo'
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "jPhone"
+    assert product.nome_da_empresa == "Orange"
+    assert product.data_de_fabricacao == "01/02/2003"
+    assert product.data_de_validade == "01/02/2004"
+    assert product.numero_de_serie == "JKLMNO123"
+    assert product.instrucoes_de_armazenamento == "Manter afastado do fogo"
