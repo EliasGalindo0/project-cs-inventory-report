533ebaf83ffe9a82dedd334513a4e0acabe8014b
EliasGalindo0 <eliasgalindo@gmail.com>
2022-11-09 23:18:37 -0300

    feat: def main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,23 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return print("Verifique os argumentos", file=sys.stderr)
+
+    if "csv" in sys.argv[1]:
+        instance = InventoryRefactor(CsvImporter)
+        result = instance.import_data(sys.argv[1], sys.argv[2])
+        sys.stdout.write(result)
+    if "json" in sys.argv[1]:
+        instance = InventoryRefactor(JsonImporter)
+        result = instance.import_data(sys.argv[1], sys.argv[2])
+        sys.stdout.write(result)
+    if "xml" in sys.argv[1]:
+        instance = InventoryRefactor(XmlImporter)
+        result = instance.import_data(sys.argv[1], sys.argv[2])
+        sys.stdout.write(result)
